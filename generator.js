f74e773e29650596e61d0b196502f549f2cb7db6
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-06-17 21:43:14 +0000

    add to gallery appears to be working, might need to perform final touches

--- generator.js
+++ generator.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
+    let generateImgVal;
+    let newLink;
+    let gallery = $("#image-gallery h2");
     $("form").submit(function(e){
         e.preventDefault();
-        let generateImgVal = $("input[type=text][name=subject]").val();
+        generateImgVal = $("input[type=text][name=subject]").val();
         console.log(generateImgVal);
-        console.log("https://source.unsplash.com/400x250/?" + generateImgVal);
-        $("div[class='image-container loaded'] img").attr("src", "https://source.unsplash.com/400x250/?" + generateImgVal);
+        newLink = "https://source.unsplash.com/400x250/?" + generateImgVal;
+        console.log(newLink);
+        $("div[class='image-container loaded'] img").attr("src", newLink);
+    });
+
+    $(".add").click(function(){
+        console.log("clicked!");
+        console.log(newLink);
+        gallery.after($("<img>").attr("src", newLink));
     });
 });
