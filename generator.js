8cfe8744ed5f7aa3ef8c5929cda290950874b4c8
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-06-19 21:29:31 +0000

    decluttered stuff

--- generator.js
+++ generator.js
@@ -4,20 +4,12 @@ $(document).ready(function(){
     let gallery = $("#image-gallery h2");
     $("form").submit(function(e){
         e.preventDefault();
-        // generateImgVal = $("input[type=text][name=subject]").val();
         generateImgVal = $(this).find("input[type=text][name=subject]").val();
-        // console.log(generateImgVal);
         newLink = "https://source.unsplash.com/400x250/?" + generateImgVal;
-        // console.log(newLink);
-        // $("div[class='image-container loaded'] img").attr("src", newLink);
-        // console.log($(this).next().children());
-        // let formNext = $(this).next();
         $($(this).next().children().attr("src", newLink));
     });
 
     $(".add").click(function(){
-        // console.log("clicked!");
-        // console.log(newLink);
         gallery.after($("<img>").attr("src", newLink));
     });
 });
