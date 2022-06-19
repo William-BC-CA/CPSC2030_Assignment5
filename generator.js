72111ffb14e5d4120a0fcae6971de88463bb9ee2
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-06-19 21:52:37 +0000

    works according to vid, TODO: declutter code

--- generator.js
+++ generator.js
@@ -2,14 +2,21 @@ $(document).ready(function(){
     let generateImgVal;
     let newLink;
     let gallery = $("#image-gallery h2");
+    let getButton;
     $("form").submit(function(e){
         e.preventDefault();
+        // getButton = $(this).children("button[class=add]");
+        // console.log(getButton);
         generateImgVal = $(this).find("input[type=text][name=subject]").val();
         newLink = "https://source.unsplash.com/400x250/?" + generateImgVal;
         $($(this).next().children().attr("src", newLink));
     });
-
-    $(".add").click(function(){
+    $((".add")).click(function(){
+    // $(getButton).click(function(){
+        // console.log("Clicked!");
+        // console.log(".add");
+        // console.log(thisForm);
+        // console.log(thisForm.children("button"));
         gallery.after($("<img>").attr("src", newLink));
     });
 });
