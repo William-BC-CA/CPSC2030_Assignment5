5c578e5e1f36ac965b7f448505544a1f276bb54a
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-06-20 19:53:28 +0000

    removed unnecessary code

--- generator.js
+++ generator.js
@@ -5,18 +5,11 @@ $(document).ready(function(){
     let getButton;
     $("form").submit(function(e){
         e.preventDefault();
-        // getButton = $(this).children("button[class=add]");
-        // console.log(getButton);
         generateImgVal = $(this).find("input[type=text][name=subject]").val();
         newLink = "https://source.unsplash.com/400x250/?" + generateImgVal;
         $($(this).next().children().attr("src", newLink));
     });
     $((".add")).click(function(){
-    // $(getButton).click(function(){
-        // console.log("Clicked!");
-        // console.log(".add");
-        // console.log(thisForm);
-        // console.log(thisForm.children("button"));
         gallery.after($("<img>").attr("src", newLink));
     });
 });
